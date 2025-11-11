4a879af6363db10b5c25873eec703eff0c7ad78d
ScriptSparrow <jurgenjong94@gmail.com>
2025-11-11 13:19:37 +0100

    SpringBoot implementation
    Added SpringBoot

---
+++ src/test/java/com/example/spoordok_ictdt/SpoordokIctdtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spoordok_ictdt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpoordokIctdtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
