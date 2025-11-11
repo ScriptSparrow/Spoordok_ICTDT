3a0d04a9f80cc87050ee973af2c71d3a0de73cc9
ScriptSparrow <jurgenjong94@gmail.com>
2025-11-11 13:27:26 +0100

    example L1 Springboot
    An example of a message controller

---
+++ src/main/java/com/example/spoordok_ictdt/MessageController.java
@@ -0,0 +1,15 @@
+package com.example.spoordok_ictdt;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class MessageController {
+
+    @GetMapping
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
