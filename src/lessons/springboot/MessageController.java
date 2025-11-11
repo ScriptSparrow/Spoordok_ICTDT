c877457472218aa5dfb77bf3272583f6aa5e9b91
ScriptSparrow <jurgenjong94@gmail.com>
2025-11-11 13:38:06 +0100

    Restructured directories
    Restructured several directories
    Add: lessons
    Add: lessons > springboot
    Del: com
    Del: group

--- src/lessons/springboot/MessageController.java
+++ src/lessons/springboot/MessageController.java
@@ -1,4 +1,4 @@
-package com.example.spoordok_ictdt;
+package spoordok_ictdt;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
