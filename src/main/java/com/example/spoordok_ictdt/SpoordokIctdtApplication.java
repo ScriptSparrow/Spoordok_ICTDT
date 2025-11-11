05bcf4e0b33223c1d3f775ceb2fdae21cf547e89
ScriptSparrow <jurgenjong94@gmail.com>
2025-11-11 13:19:37 +0100

    SpringBoot implementation
    Added SpringBoot

---
+++ src/main/java/com/example/spoordok_ictdt/SpoordokIctdtApplication.java
@@ -0,0 +1,13 @@
+package com.example.spoordok_ictdt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpoordokIctdtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpoordokIctdtApplication.class, args);
+    }
+
+}
