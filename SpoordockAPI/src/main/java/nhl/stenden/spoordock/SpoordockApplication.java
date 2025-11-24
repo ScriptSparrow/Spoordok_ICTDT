4eb094564c7ae8ec08d885d2e2a68b86da1c8529
dutchie031 <dutchie032>
2025-11-24 20:34:18 +0100

    initial api setup

---
+++ SpoordockAPI/src/main/java/nhl/stenden/spoordock/SpoordockApplication.java
@@ -0,0 +1,13 @@
+package nhl.stenden.spoordock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpoordockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpoordockApplication.class, args);
+	}
+
+}
