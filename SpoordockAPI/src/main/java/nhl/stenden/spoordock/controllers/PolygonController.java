3765d0ba58b7bbf8497e0c01b8996c69756ebfdc
CookieMonsterXL <rvanopbergen@outlook.com>
2025-11-25 12:38:33 +0100

    Initiele commit
    wop
    classes en controllers setup

---
+++ SpoordockAPI/src/main/java/nhl/stenden/spoordock/controllers/PolygonController.java
@@ -0,0 +1,17 @@
+public class PolygonController {
+    // De polygon controller is verantwoordelijk om aanpassingen door te voeren en polygon gegevens terug te geven
+
+    /*  GET /polygon/list
+            -> Polygon[]
+
+        POST /polygon
+            [FromBody] Polygon
+            -> Polygon
+
+        PUT /polygon/{id}
+            [FromBody] Polygon
+            -> Polygon
+
+        DELETE /polygon/{id}
+     */
+}
---
+++ SpoordockAPI/src/main/java/nhl/stenden/spoordock/controllers/TypeController.java
@@ -0,0 +1,5 @@
+public class TypeController {
+    // De TypeController is verantwoordelijk voor het exposen van endpoints t.a.v. GebouwTypes
+
+
+}
