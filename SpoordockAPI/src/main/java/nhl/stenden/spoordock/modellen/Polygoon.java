303f1251e9cefd0da90b7beed0ac18881f9838f0
CookieMonsterXL <rvanopbergen@outlook.com>
2025-12-05 12:20:23 +0100

    Road feature vanaf database tot aan controller gemaakt.
    Packages & Classes toegevoegd:
    controllers
    	controllers/dtos
    		controllers/dto/RoadDTO
    	controllers/RoadController
    
    database
    	database/entities
    		database/entities/RoadSegment
    		database/entities/RoadTypeTemplate
    	database/RoadTypeRepository
    
    sevices
    	services/mappers
    		services/mappers/Mapper
    	services/RoadService

--- SpoordockAPI/src/main/java/nhl/stenden/spoordock/modellen/Polygoon.java
+++
@@ -1,8 +0,0 @@
-public class Polygoon {
-    // Een polygoon is een object dat de "zone" aangeeft waarin een type gebouw(en) moet worden geplaatst
-
-    private int id;
-    // private type: GebouwTypeTemplate
-    // private punten: Vec3[]
-    private double hoogte; // mogelijk woonlagen of verdiepingen een betere benaming?
-}
