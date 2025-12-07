a8ddb9e38669f83bb90501e99224ea8503aa660a
dutchie031 <dutchie032>
2025-12-07 12:29:09 +0100

    wip, initial implementation of Polygones as an example

---
+++ SpoordockAPI/src/main/java/nhl/stenden/spoordock/controllers/dtos/BuildingTypeDTO.java
@@ -0,0 +1,20 @@
+package nhl.stenden.spoordock.controllers.dtos;
+
+import java.util.UUID;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class BuildingTypeDTO {
+
+    private UUID buildingTypeId;
+    private String name;
+    private String description;
+    private double floorHeight;
+    private String color;
+
+}
