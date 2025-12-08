1f102a26350588861530e9b33035725d95984ec8
dutchie031 <dutchie032>
2025-12-08 13:10:11 +0100

    semantic search and LLM reasoning possible

---
+++ SpoordockAPI/src/main/java/nhl/stenden/spoordock/llmService/dtos/EmbeddingResponseBody.java
@@ -0,0 +1,17 @@
+package nhl.stenden.spoordock.llmService.dtos;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class EmbeddingResponseBody {
+    private String model;
+    private float[][] embedding;
+
+}
