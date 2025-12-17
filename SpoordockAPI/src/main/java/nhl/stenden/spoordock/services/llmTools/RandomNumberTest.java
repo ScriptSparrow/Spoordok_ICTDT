2f6b50a86a4af9a38626b4aef95599fb348ff1be
dutchie031 <timrorije@gmail.com>
2025-12-17 15:58:23 +0100

    wip

--- SpoordockAPI/src/main/java/nhl/stenden/spoordock/services/llmTools/RandomNumberTest.java
+++ SpoordockAPI/src/main/java/nhl/stenden/spoordock/services/llmTools/RandomNumberTest.java
@@ -16,7 +16,7 @@ public class RandomNumberTest implements ToolService  {
     public int generateRandomNumber(
             @ToolParameter(description = "The minimum value (inclusive).") int min,
             @ToolParameter(description = "The maximum value (inclusive).") int max
-    ) 
+    )
     {
         return (int) (Math.random() * (max - min + 1)) + min;
     }
