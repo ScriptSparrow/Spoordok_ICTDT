a676659b08343fb7f0112eb76ea755e623b5306a
dutchie031 <timrorije@gmail.com>
2026-01-13 00:12:15 +0100

    updated front-end stuff to connect to backend, still needs work

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite';
 import cesium from 'vite-plugin-cesium';
 
 export default defineConfig({
-  plugins: [cesium()]
+  plugins: [cesium()],
+  server : {
+    host: '0.0.0.0',
+    port: 5173
+  }
 });
