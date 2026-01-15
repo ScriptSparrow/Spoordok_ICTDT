040100de0914b9685be057d8e31e6fe3310b1fb6
dutchie031 <dutchie032>
2026-01-15 11:39:54 +0100

    wip

--- SpoordockAPI/src/main/java/nhl/stenden/spoordock/controllers/dtos/BuildingTypeDTO.java
+++ SpoordockAPI/src/main/java/nhl/stenden/spoordock/controllers/dtos/BuildingTypeDTO.java
@@ -2,6 +2,7 @@ package nhl.stenden.spoordock.controllers.dtos;
 
 import java.util.UUID;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,12 +10,34 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class BuildingTypeDTO {
 
+    // Identifier
     private UUID buildingTypeId;
-    private String name;
+
+    // LabelName in Human readable format
+    private String labelName;
+
+    // Description of the building type
     private String description;
-    private double floorHeight;
+
+    // unit (m3, m2 etc)
+    private String unit;
+
+    //Cost per unit
+    private String costPerUnit;
+
+    // Is the building inhabitable
+    private boolean inhabitable;
+
+    // number of residents per unit
+    private Double residentsPerUnit;
+
+    // points awarded for building this type per unit
+    private int points;    
+
+    // Color representation in the frontend in hex format
     private String color;
 
 }
